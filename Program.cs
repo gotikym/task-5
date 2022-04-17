76f9ef413485182cb4a0e81d1222f1e03dff4275
One <One@DESKTOP-MIB22JC>
2022-04-17 21:46:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        var firstName = "Malkovskii";
+        var secondName = "Ivan";
+        Console.WriteLine(firstName + " " + secondName);
+        var switchName = secondName;
+        secondName = firstName;
+        firstName = switchName;
+        Console.WriteLine(firstName + " " + secondName);
+        }
+    }
